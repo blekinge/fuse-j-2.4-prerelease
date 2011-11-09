0fb301bd6c49546e7ca405131c60acf9da36cdaa
Asger Askov Blekinge <abr@statsbiblioteket.dk>
2011-11-09 16:33:25 +0100

    Mavenized the project

---
+++ zipfs/src/main/script/zipfs_mount_mt.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+#
+# Usage: ./zipfs_mount.sh file.zip /mount/point
+
+(
+    HOME="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+    echo "Script home dir $HOME"
+
+    echo "Using this JDK_HOME=$JDK_HOME"
+
+	[[ "$3" == "-d" ]] && DEBUG="-Dorg.apache.commons.logging.Log=fuse.logging.FuseLog -Dfuse.logging.level=DEBUG"
+	
+	LD_LIBRARY_PATH=$HOME/jni:$HOME/lib $JDK_HOME/bin/java \
+		-cp "$HOME/lib/*" \
+		$DEBUG fuse.zipfs.MultiZipFilesystem \
+		-f -s "$2" "$1"
+) &
